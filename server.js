abdefd6b5a8fda016bbfaeed92fb622bc221fa3b
marouane <marouane@marouanes-MacBook-Pro.local>
2023-07-18 22:56:39 +0100

    Update app name

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-app.use(express.static('./dist/tagspredictapp'));
+app.use(express.static('./dist/dashboard'));
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/tagspredictapp/'}
+  res.sendFile('index.html', {root: 'dist/dashboard/'}
 );
 });
 app.listen(process.env.PORT || 8080);
